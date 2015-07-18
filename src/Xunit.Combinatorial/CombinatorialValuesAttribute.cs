54243f75ae76bc85a7f92e2e488ffe61cafb985f
Andrew Arnott <andarno@microsoft.com>
2015-07-18 15:53:30 -0700

    Apply StyleCop to library project

--- src/Xunit.Combinatorial/CombinatorialValuesAttribute.cs
+++ src/Xunit.Combinatorial/CombinatorialValuesAttribute.cs
@@ -3,9 +3,16 @@
     using System;
     using Validation;
 
+    /// <summary>
+    /// Specifies which values for this parameter should be used for running the test method.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Parameter)]
     public class CombinatorialValuesAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CombinatorialValuesAttribute"/> class.
+        /// </summary>
+        /// <param name="values">The values to pass to this parameter.</param>
         public CombinatorialValuesAttribute(params object[] values)
         {
             Requires.NotNull(values, nameof(values));
@@ -13,6 +20,10 @@
             this.Values = values;
         }
 
+        /// <summary>
+        /// Gets the values that should be passed to this parameter on the test method.
+        /// </summary>
+        /// <value>An array of values.</value>
         public object[] Values { get; }
     }
 }
