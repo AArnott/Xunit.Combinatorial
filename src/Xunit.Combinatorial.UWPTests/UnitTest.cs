169422558acc0ba528ecfc261d58bf8a77d69027
Andrew Arnott <andarno@microsoft.com>
2015-12-02 21:29:46 -0800

    Add UWP test projects

---
+++ src/Xunit.Combinatorial.UWPTests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void OrdinaryFact()
+    {
+    }
+
+    [Theory, CombinatorialData]
+    public void TestMethod1(bool hi)
+    {
+    }
+}
