971fd8bd4b7f4f40c658b408b596961b0e9e306b
Andrew Arnott <andarno@microsoft.com>
2015-12-05 16:16:39 -0800

    Add sample uses to compiled code

---
+++ src/Xunit.Combinatorial.Tests/SampleUses.cs
@@ -0,0 +1,36 @@
+﻿namespace Xunit.Combinatorial.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class SampleUses
+    {
+        [Theory, CombinatorialData]
+        public void CombinatorialBoolean(bool v1, bool v2, bool v3)
+        {
+            // Combinatorial generates these 8 test cases:
+            // false false false
+            // false false true
+            // false true  false
+            // false true  true
+            // true  false false
+            // true  false true
+            // true  true  false
+            // true  true  true
+        }
+
+        [Theory, PairwiseData]
+        public void PairwiseBoolean(bool v1, bool v2, bool v3)
+        {
+            // Pairwise generates these 4 test cases:
+            // false false false
+            // false true  true
+            // true  false true
+            // true  true  false
+        }
+    }
+}
