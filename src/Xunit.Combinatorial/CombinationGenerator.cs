562bf403875b2d6ec555939f0e2f3d9b40ebd9a2
Matt Wilkinson <matthew.wilkinson@gmail.com>
2017-04-21 19:39:47 +0100

    Improve performance of combination generator with manual nested loops for parameter counts up to 10
    Nested for-loops result in simple iterators and under performance testing were significantly faster as number of parameters/combinations increased. For low numbers of parameters (<= 4), performance is no worse.
    Falls back to a recursive generator (with a fast-loop once recursed to 10 parameters left)

---
+++ src/Xunit.Combinatorial/CombinationGenerator.cs
@@ -0,0 +1,190 @@
+﻿namespace Xunit
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal static class CombinationGenerator
+    {
+        public static IEnumerable<object[]> Generate(params object[][] values) => Generate(values, 0);
+
+        private static IEnumerable<object[]> Generate(object[][] values, int depth)
+        {
+            // If we're at the last 10 parameter elements, we can use the fast generator for the last 10 parameters
+            if (values.Length - depth == 10)
+            {
+#pragma warning disable SA1519 // Braces must not be omitted from multi-line child statement
+                for (int a = 0; a < values[depth + 0].Length; a++)
+                for (int b = 0; b < values[depth + 1].Length; b++)
+                for (int c = 0; c < values[depth + 2].Length; c++)
+                for (int d = 0; d < values[depth + 3].Length; d++)
+                for (int e = 0; e < values[depth + 4].Length; e++)
+                for (int f = 0; f < values[depth + 5].Length; f++)
+                for (int g = 0; g < values[depth + 6].Length; g++)
+                for (int h = 0; h < values[depth + 7].Length; h++)
+                for (int i = 0; i < values[depth + 8].Length; i++)
+                for (int j = 0; j < values[depth + 9].Length; j++)
+                {
+                    var comboArr = new object[values.Length];
+                    comboArr[values.Length - 10] = values[depth + 0][a];
+                    comboArr[values.Length - 9] = values[depth + 1][b];
+                    comboArr[values.Length - 8] = values[depth + 2][c];
+                    comboArr[values.Length - 7] = values[depth + 3][d];
+                    comboArr[values.Length - 6] = values[depth + 4][e];
+                    comboArr[values.Length - 5] = values[depth + 5][f];
+                    comboArr[values.Length - 4] = values[depth + 6][g];
+                    comboArr[values.Length - 3] = values[depth + 7][h];
+                    comboArr[values.Length - 2] = values[depth + 8][i];
+                    comboArr[values.Length - 1] = values[depth + 9][j];
+                    yield return comboArr;
+                }
+#pragma warning restore SA1519 // Braces must not be omitted from multi-line child statement
+            }
+            else
+            {
+                var thisDepthArr = values[depth];
+                foreach (var combo in Generate(values, depth + 1))
+                {
+                    for (int i = 0; i < thisDepthArr.Length - 1; i++)
+                    {
+                        var newComboArr = new object[values.Length];
+                        Array.Copy(combo, depth, newComboArr, depth, values.Length - depth);
+                        newComboArr[depth] = thisDepthArr[i];
+                        yield return newComboArr;
+                    }
+
+                    combo[depth] = thisDepthArr[thisDepthArr.Length - 1];
+                    yield return combo;
+                }
+            }
+        }
+
+#pragma warning disable SA1519 // Braces must not be omitted from multi-line child statement
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            {
+                yield return new[] { pos0Values[a] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values, object[] pos4Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            for (int e = 0; e < pos4Values.Length; e++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d], pos4Values[e] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values, object[] pos4Values, object[] pos5Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            for (int e = 0; e < pos4Values.Length; e++)
+            for (int f = 0; f < pos5Values.Length; f++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d], pos4Values[e], pos5Values[f] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values, object[] pos4Values, object[] pos5Values, object[] pos6Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            for (int e = 0; e < pos4Values.Length; e++)
+            for (int f = 0; f < pos5Values.Length; f++)
+            for (int g = 0; g < pos6Values.Length; g++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d], pos4Values[e], pos5Values[f], pos6Values[g] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values, object[] pos4Values, object[] pos5Values, object[] pos6Values, object[] pos7Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            for (int e = 0; e < pos4Values.Length; e++)
+            for (int f = 0; f < pos5Values.Length; f++)
+            for (int g = 0; g < pos6Values.Length; g++)
+            for (int h = 0; h < pos7Values.Length; h++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d], pos4Values[e], pos5Values[f], pos6Values[g], pos7Values[h] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values, object[] pos4Values, object[] pos5Values, object[] pos6Values, object[] pos7Values, object[] pos8Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            for (int e = 0; e < pos4Values.Length; e++)
+            for (int f = 0; f < pos5Values.Length; f++)
+            for (int g = 0; g < pos6Values.Length; g++)
+            for (int h = 0; h < pos7Values.Length; h++)
+            for (int i = 0; i < pos8Values.Length; i++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d], pos4Values[e], pos5Values[f], pos6Values[g], pos7Values[h], pos8Values[i] };
+            }
+        }
+
+        public static IEnumerable<object[]> Generate(object[] pos0Values, object[] pos1Values, object[] pos2Values, object[] pos3Values, object[] pos4Values, object[] pos5Values, object[] pos6Values, object[] pos7Values, object[] pos8Values, object[] pos9Values)
+        {
+            for (int a = 0; a < pos0Values.Length; a++)
+            for (int b = 0; b < pos1Values.Length; b++)
+            for (int c = 0; c < pos2Values.Length; c++)
+            for (int d = 0; d < pos3Values.Length; d++)
+            for (int e = 0; e < pos4Values.Length; e++)
+            for (int f = 0; f < pos5Values.Length; f++)
+            for (int g = 0; g < pos6Values.Length; g++)
+            for (int h = 0; h < pos7Values.Length; h++)
+            for (int i = 0; i < pos8Values.Length; i++)
+            for (int j = 0; j < pos9Values.Length; j++)
+            {
+                yield return new[] { pos0Values[a], pos1Values[b], pos2Values[c], pos3Values[d], pos4Values[e], pos5Values[f], pos6Values[g], pos7Values[h], pos8Values[i], pos9Values[j] };
+            }
+        }
+
+#pragma warning restore SA1519 // Braces must not be omitted from multi-line child statement
+    }
+}
--- src/Xunit.Combinatorial/CombinatorialDataAttribute.cs
+++ src/Xunit.Combinatorial/CombinatorialDataAttribute.cs
@@ -6,9 +6,6 @@ namespace Xunit
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Xunit;
     using Xunit.Sdk;
 
     /// <summary>
@@ -25,59 +22,34 @@ namespace Xunit
             Requires.NotNull(testMethod, nameof(testMethod));
 
             var parameters = testMethod.GetParameters();
-            if (parameters.Length == 0)
+            switch (parameters.Length)
             {
-                return Enumerable.Empty<object[]>();
+                case 0: return Enumerable.Empty<object[]>();
+                case 1: return CombinationGenerator.Generate(Values(parameters[0]));
+                case 2: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]));
+                case 3: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]));
+                case 4: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]));
+                case 5: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]), Values(parameters[4]));
+                case 6: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]), Values(parameters[4]), Values(parameters[5]));
+                case 7: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]), Values(parameters[4]), Values(parameters[5]), Values(parameters[6]));
+                case 8: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]), Values(parameters[4]), Values(parameters[5]), Values(parameters[6]), Values(parameters[7]));
+                case 9: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]), Values(parameters[4]), Values(parameters[5]), Values(parameters[6]), Values(parameters[7]), Values(parameters[8]));
+                case 10: return CombinationGenerator.Generate(Values(parameters[0]), Values(parameters[1]), Values(parameters[2]), Values(parameters[3]), Values(parameters[4]), Values(parameters[5]), Values(parameters[6]), Values(parameters[7]), Values(parameters[8]), Values(parameters[9]));
+                default: return GenerateCombinations(parameters);
             }
-
-            var values = new List<object>[parameters.Length];
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                values[i] = ValuesUtilities.GetValuesFor(parameters[i]).ToList();
-            }
-
-            var currentValues = new object[parameters.Length];
-            return this.FillCombinations(parameters, values, currentValues, 0);
         }
 
-        /// <summary>
-        /// Produces a sequence of argument arrays that capture every possible
-        /// combination of values.
-        /// </summary>
-        /// <param name="parameters">The parameters taken by the test method.</param>
-        /// <param name="candidateValues">An array of each argument's list of possible values.</param>
-        /// <param name="currentValues">An array that is being recursively initialized with a set of arguments to pass to the test method.</param>
-        /// <param name="index">The index into <paramref name="currentValues"/> that this particular invocation should rotate through <paramref name="candidateValues"/> for.</param>
-        /// <returns>A sequence of all combinations of arguments from <paramref name="candidateValues"/>, starting at <paramref name="index"/>.</returns>
-        private IEnumerable<object[]> FillCombinations(ParameterInfo[] parameters, List<object>[] candidateValues, object[] currentValues, int index)
+        private static object[] Values(ParameterInfo param) => ValuesUtilities.GetValuesFor(param);
+
+        private static IEnumerable<object[]> GenerateCombinations(ParameterInfo[] parameters)
         {
-            Requires.NotNull(parameters, nameof(parameters));
-            Requires.NotNull(candidateValues, nameof(candidateValues));
-            Requires.NotNull(currentValues, nameof(currentValues));
-            Requires.Argument(parameters.Length == candidateValues.Length, nameof(candidateValues), $"Expected to have same array length as {nameof(parameters)}");
-            Requires.Argument(parameters.Length == currentValues.Length, nameof(currentValues), $"Expected to have same array length as {nameof(parameters)}");
-            Requires.Range(index >= 0 && index < parameters.Length, nameof(index));
-
-            foreach (object value in candidateValues[index])
+            var values = new object[parameters.Length][];
+            for (int i = 0; i < parameters.Length; i++)
             {
-                currentValues[index] = value;
-
-                if (index + 1 < parameters.Length)
-                {
-                    foreach (object[] result in this.FillCombinations(parameters, candidateValues, currentValues, index + 1))
-                    {
-                        yield return result;
-                    }
-                }
-                else
-                {
-                    // We're the tail end, so just produce the value.
-                    // Copy the array before returning since we're about to mutate currentValues
-                    object[] finalSet = new object[currentValues.Length];
-                    Array.Copy(currentValues, finalSet, currentValues.Length);
-                    yield return finalSet;
-                }
+                values[i] = Values(parameters[i]);
             }
+
+            return CombinationGenerator.Generate(values);
         }
     }
 }
