cc0cfa47c52e6fd31e02a6c6da373d7d535e00d3
Andrew Miller <andrew.miller@myriad.com>
2020-02-24 16:19:33 -0500

    Added CombinatorialRangeAttribute class

---
+++ src/Xunit.Combinatorial/CombinatorialRangeAttribute.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved. Licensed under the Ms-PL.
+
+namespace Xunit
+{
+    using System;
+
+    /// <summary>
+    /// Specifies which range of values for this parameter should be used for running the test method.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class CombinatorialRangeAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CombinatorialRangeAttribute"/> class.
+        /// </summary>
+        /// <param name="from">The value at the beginning of the range.</param>
+        /// <param name="count">The quantity of consecutive integer values to include.</param>
+        public CombinatorialRangeAttribute(int from, int count)
+        {
+            if (count < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            object[] values = new object[count];
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = from + i;
+            }
+
+            this.Values = values;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CombinatorialRangeAttribute"/> class.
+        /// </summary>
+        /// <param name="from">The value at the beginning of the range.</param>
+        /// <param name="to">The value at the end of the range.</param>
+        /// <param name="step">The number of integers to step for each value in result.</param>
+        public CombinatorialRangeAttribute(int from, int to, int step)
+        {
+            // to cannot be less than from
+            if (to < from)
+            {
+                throw new ArgumentOutOfRangeException(nameof(to));
+            }
+
+            // step must be a positive integer
+            if (step < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(step));
+            }
+
+            int count = (to - from) / step;
+            object[] values = new object[count];
+            for (int i = 0; i < count; i += step)
+            {
+                values[i] = from + i;
+            }
+
+            this.Values = values;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CombinatorialRangeAttribute"/> class.
+        /// </summary>
+        /// <param name="from">The value at the beginning of the range.</param>
+        /// <param name="to">The value at the end of the range.</param>
+        /// <param name="step">The amount to skip for each value in result.</param>
+        public CombinatorialRangeAttribute(double from, double to, double step)
+        {
+            if (to < from)
+            {
+                throw new ArgumentOutOfRangeException(nameof(to));
+            }
+
+            if (step <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(step));
+            }
+
+            // Explicit cast truncates
+            int count = (int)((to - from) / step);
+            
+            object[] values = new object[count];
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = from + (i * step);
+            }
+
+            this.Values = values;
+        }
+
+        /// <summary>
+        /// Gets the values that should be passed to this parameter on the test method.
+        /// </summary>
+        /// <value>An array of values.</value>
+        public object[] Values { get; }
+    }
+}
